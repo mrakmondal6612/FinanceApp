f9eef8d76d06ec7550ee9c6f3529c0b08d6abf02
Ajay Mondal <121559193+mrakmondal6612@users.noreply.github.com>
2024-10-08 10:44:35 +0530

    Update Merchant Models

--- models/Merchant.js
+++ models/Merchant.js
@@ -1,16 +1,44 @@
 const mongoose = require('mongoose');
 
 const merchantSchema = new mongoose.Schema({
-  name: String,
-  businessName: String,
-  email: String,
-  password: String,
-  sales: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Sale',
-    },
-  ],
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  storeName: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    match: /^\d{10}$/,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\S+@\S+\.\S+$/
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  storeAddress: {
+    type: String,
+    maxLength: 255
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Merchant', merchantSchema);
